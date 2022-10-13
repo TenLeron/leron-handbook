23a6411e20c0539b1490993cede4bc3cf994ccb0
TenLeron <66697847+TenLeron@users.noreply.github.com>
2022-10-13 15:45:02 +0200

    Added geometric calculator

--- main.cpp
+++ main.cpp
@@ -1,39 +1,70 @@
-// Základní instance třídy - zapsání do ní - vypsání informací z třídy
-
 #include <iostream>
 using namespace std;
 
-class Student {
-public:
-    double id;
-    string name;
-    int age;
-    int semester;
+int vyber, cislo1, cislo2, vysledekO, vysledekS;
 
-    void printInfo() {
-        cout << "ID: " << id << endl;
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Semester: " << semester << endl;
+void ctverec() {
+    cout << "==============" << endl;
+    cout << "Byl zvolen čtverec" << endl;
+    cout << "Zadejte stranu A: ";
+    cin >> cislo1;
+    while (cislo1 <= 0) {
+        cout << "Byla zadána záporná hodnota - s tou nejde počítat" << endl;
+        cout << "Opakujte zadání: ";
+        cin >> cislo1;
     }
-};
+    vysledekO = 4 * cislo1;
+    vysledekS = cislo1 * cislo1;
+    cout << "Obvod: " << vysledekO << endl;
+    cout << "Obsah: " << vysledekS << endl;
+    return;
+}
+void obdelnik() {
+    cout << "==============" << endl;
+    cout << "Byl zvolen obdelník" << endl;
+    cout << "Zadejte stranu A: ";
+    cin >> cislo1;
+    cout << "Zadejte stranu B: ";
+    cin >> cislo2;
+
+    while (cislo1 <= 0 || cislo2 <= 0) {
+        cout << "Byla zadána záporná hodnota - s tou nejde počítat" << endl;
+        if (cislo1 <= 0) {
+            cout << "Zadejte znovu stranu A: ";
+            cin >> cislo1;
+        }
+        else {
+            cout << "Zadejte znovu stranu B: ";
+            cin >> cislo2;
+        }
+    }
+    vysledekO = 2* (cislo1 + cislo2);
+    vysledekS = cislo1 * cislo2;
+    cout << "Obvod: " << vysledekO << endl;
+    cout << "Obsah: " << vysledekS << endl;
+    return;
+}
+
+
 
 int main() {
+    cout << "Geometrická kalkulačka pro základní útvary" << endl;
+    cout << "Vyberte si možnost počítání: " << endl;
+    cout << "1. Čtverec" << endl;
+    cout << "2. Obdelník" << endl;
+    cout << "Váš výběr: ";
 
-    Student* Jakub = new Student();
-    Jakub -> id = 0001;
-    Jakub -> name = "Jakub Solař";
-    Jakub -> age = 5;
-    Jakub -> semester = 1;
-
-    Student* Denisa = new Student();
-    Denisa -> id = 0002;
-    Denisa -> name = "Denisa Nedomová";
-    Denisa -> age = 45;
-    Denisa -> semester = 2;
-
-    Jakub -> printInfo();
-    cout << "===============" << endl;
-    Denisa -> printInfo();
-
+    cin >> vyber;
+    switch (vyber) {
+        case 1:
+            ctverec();
+            break;
+        case 2:
+            obdelnik();
+            break;
+        default:
+            cout << "Nebyla vybrána žádná možnost..." << endl;
+            break;
+    }
+    return 0;
 }
