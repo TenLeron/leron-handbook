9a61f0d51d72dc4e36397f85d6a19b20cd042914
TenLeron <66697847+TenLeron@users.noreply.github.com>
2022-10-13 15:37:40 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+class Student {
+public:
+    double id;
+    string name;
+    int age;
+    int semester;
+
+    void printInfo() {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Semester: " << semester << endl;
+    }
+};
+
+int main() {
+
+    Student* Jakub = new Student();
+    Jakub -> id = 0001;
+    Jakub -> name = "Jakub Solař";
+    Jakub -> age = 5;
+    Jakub -> semester = 1;
+
+    Student* Denisa = new Student();
+    Denisa -> id = 0002;
+    Denisa -> name = "Denisa Nedomová";
+    Denisa -> age = 45;
+    Denisa -> semester = 2;
+
+    Jakub -> printInfo();
+    cout << "===============" << endl;
+    Denisa -> printInfo();
+
+}
